b2a802b050bc9f14558469b44f324c209968150c
Sam Hastings <samhastingsis@gmail.com>
2023-11-02 18:03:42 -0400

    Initial Commit

---
+++ SetSwiftUi/SetSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetSwiftUiApp.swift
+//  SetSwiftUi
+//
+//  Created by sam hastings on 02/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct SetSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
